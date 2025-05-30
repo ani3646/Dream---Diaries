dbe5e9fecc7ff4d5a3e0f39609b6f0e618d5b1e7
ani3646 <ankitdixit3646@gmail.com>
2025-05-30 23:52:37 +0530

    Initial Stage

---
+++ models/listing.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ListingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: "No description provided",
+  },
+  image: {
+    filename: String,
+    url: {
+      type: String,
+      default:
+        "https://images.unsplash.com/photo-1590523278191-995cbcda646b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1080&q=80",
+    },
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: [0, "Price cannot be negative"],
+  },
+  location: {
+    type: String,
+    default: "Unknown location",
+  },
+  country: {
+    type: String,
+    default: "Unknown country",
+  },
+});
+
+const Listing = mongoose.model("Listing", ListingSchema);
+module.exports = Listing;
